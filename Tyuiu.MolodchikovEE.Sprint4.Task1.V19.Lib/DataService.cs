142cd033702f871ccf4c17a727459145bcdbedbc
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2024-09-26 22:15:52 +0500

    Task1

--- Tyuiu.MolodchikovEE.Sprint4.Task1.V19.Lib/DataService.cs
+++ Tyuiu.MolodchikovEE.Sprint4.Task1.V19.Lib/DataService.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.MolodchikovEE.Sprint4.Task1.V19.Lib
 
             for (int i = 0; i < array.Length - 1; i++)
             {
-                if (array[i] == 0) 
+                if (i %2 !=0) 
                 {
                     sum += array[i];
                 }
